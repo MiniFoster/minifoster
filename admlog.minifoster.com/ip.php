6ddb2a4984f9b48b6b38df186dc791d15d643ae0
MiniFoster <85492704+MiniFoster@users.noreply.github.com>
2021-06-07 17:02:01 +0530

    Add files via upload

---
+++ admlog.minifoster.com/ip.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php $ipaddresss = $_SERVER['REMOTE_ADDR']; echo "Your IP Address is " .$ipaddresss; ?>
+</body>
+</html> 
---
+++ php/connect.php
@@ -0,0 +1,12 @@
+<?php
+require_once('dblog');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
